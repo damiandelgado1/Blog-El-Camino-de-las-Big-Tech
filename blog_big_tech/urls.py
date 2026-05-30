49b62aac67fb92c79d01f13894b92676fe5ef6fd
Damian <damian.delgadogallardo01@gmail.com>
2026-05-30 13:25:04 -0400

    Add style CSS a website and every blog

--- blog_big_tech/urls.py
+++ blog_big_tech/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from .views import main_page, form_suscribe
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('category/', include('category.urls', namespace="category")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
